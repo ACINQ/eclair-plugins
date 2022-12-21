e3500ee42f6f11cd009928d5d5ca6ffaec053b15
t-bast <bastuc@hotmail.fr>
2022-12-21 17:53:18 +0100

    Fix build
    Update dependencies to match latest eclair.
    Fix types in the gossip plugin.

--- eclair-gossip-plugin/src/main/scala/fr/acinq/eclair/plugins/gossip/AppendOnlyNetworkDb.scala
+++ eclair-gossip-plugin/src/main/scala/fr/acinq/eclair/plugins/gossip/AppendOnlyNetworkDb.scala
@@ -1,55 +1,71 @@
+/*
+ * Copyright 2022 ACINQ SAS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fr.acinq.eclair.plugins.gossip
 
-import java.sql.Connection
-
-import fr.acinq.bitcoin.{ByteVector32, Satoshi}
+import fr.acinq.bitcoin.scalacompat.Satoshi
 import fr.acinq.eclair.db.sqlite.SqliteUtils
-import fr.acinq.eclair.router.Announcements
-import fr.acinq.eclair.wire.{ChannelAnnouncement, ChannelUpdate, NodeAnnouncement}
+import fr.acinq.eclair.wire.protocol.LightningMessageCodecs.{channelAnnouncementCodec, channelUpdateCodec, nodeAnnouncementCodec}
+import fr.acinq.eclair.wire.protocol.{ChannelAnnouncement, ChannelUpdate, NodeAnnouncement}
+
+import java.sql.Connection
 
 class AppendOnlyNetworkDb(sqlite: Connection) {
 
   import SqliteUtils._
 
-
   using(sqlite.createStatement()) { statement =>
-    statement.executeUpdate("CREATE TABLE IF NOT EXISTS nodes (node_id BLOB NOT NULL, timestamp INTEGER NOT NULL)")
-    statement.executeUpdate("CREATE TABLE IF NOT EXISTS channels (short_channel_id INTEGER NOT NULL, node_id_1 BLOB NOT NULL, node_id_2 BLOB NOT NULL, capacity_sat INTEGER NOT NULL)")
-    statement.executeUpdate("CREATE TABLE IF NOT EXISTS updates (short_channel_id INTEGER NOT NULL, timestamp INTEGER NOT NULL, message_flags INTEGER NOT NULL, channel_flags INTEGER NOT NULL, cltv_expiry_delta INTEGER NOT NULL, htlc_minimum_msat INTEGER NOT NULL, fee_base_msat INTEGER NOT NULL, fee_proportional_millionths INTEGER NOT NULL, htlc_maximum_msat INTEGER)")
+    statement.executeUpdate("CREATE TABLE IF NOT EXISTS nodes (node_id TEXT NOT NULL, data BLOB NOT NULL, timestamp INTEGER NOT NULL)")
+    statement.executeUpdate("CREATE TABLE IF NOT EXISTS channels (short_channel_id TEXT NOT NULL, node_id_1 TEXT NOT NULL, node_id_2 TEXT NOT NULL, capacity_sat INTEGER NOT NULL, channel_announcement BLOB NOT NULL)")
+    statement.executeUpdate("CREATE TABLE IF NOT EXISTS updates (short_channel_id TEXT NOT NULL, dont_forward INTEGER NOT NULL, is_node_1 INTEGER NOT NULL, is_enabled INTEGER NOT NULL, cltv_expiry_delta INTEGER NOT NULL, fee_base_msat INTEGER NOT NULL, fee_proportional_millionths INTEGER NOT NULL, htlc_minimum_msat INTEGER NOT NULL, htlc_maximum_msat INTEGER NOT NULL, timestamp INTEGER NOT NULL, channel_update BLOB NOT NULL)")
   }
 
   def addNode(n: NodeAnnouncement): Unit = {
-    using(sqlite.prepareStatement("INSERT OR IGNORE INTO nodes VALUES (?, ?)")) { statement =>
-      statement.setBytes(1, n.nodeId.toBin.toArray)
-      statement.setLong(2, n.timestamp)
+    using(sqlite.prepareStatement("INSERT OR IGNORE INTO nodes VALUES (?, ?, ?)")) { statement =>
+      statement.setString(1, n.nodeId.toHex)
+      statement.setBytes(2, nodeAnnouncementCodec.encode(n).require.toByteArray)
+      statement.setLong(3, n.timestamp.toLong)
       statement.executeUpdate()
     }
   }
 
   def addChannel(c: ChannelAnnouncement, capacity: Satoshi): Unit = {
-    using(sqlite.prepareStatement("INSERT OR IGNORE INTO channels VALUES (?, ?, ?, ?)")) { statement =>
-      statement.setLong(1, c.shortChannelId.toLong)
-      statement.setBytes(2, c.nodeId1.toArray)
-      statement.setBytes(3, c.nodeId2.toArray)
-      statement.setLong(4, capacity.amount)
+    using(sqlite.prepareStatement("INSERT OR IGNORE INTO channels VALUES (?, ?, ?, ?, ?)")) { statement =>
+      statement.setString(1, c.shortChannelId.toString)
+      statement.setString(2, c.nodeId1.toHex)
+      statement.setString(3, c.nodeId2.toHex)
+      statement.setLong(4, capacity.toLong)
+      statement.setBytes(5, channelAnnouncementCodec.encode(c).require.toByteArray)
       statement.executeUpdate()
     }
   }
 
   def addUpdate(u: ChannelUpdate): Unit = {
-    using(sqlite.prepareStatement("INSERT OR IGNORE INTO updates VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)")) { statement =>
-      statement.setLong(1, u.shortChannelId.toLong)
-      statement.setLong(2, u.timestamp)
-      statement.setByte(3, u.messageFlags)
-      statement.setByte(4, u.channelFlags)
-      statement.setInt(5, u.cltvExpiryDelta)
-      statement.setLong(6, u.htlcMinimumMsat)
-      statement.setLong(7, u.feeBaseMsat)
-      statement.setLong(8, u.feeProportionalMillionths)
-      u.htlcMaximumMsat match {
-        case Some(value) => statement.setLong(9, value)
-        case None => statement.setNull(9, java.sql.Types.INTEGER)
-      }
+    using(sqlite.prepareStatement("INSERT OR IGNORE INTO updates VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) { statement =>
+      statement.setString(1, u.shortChannelId.toString)
+      statement.setBoolean(2, u.messageFlags.dontForward)
+      statement.setBoolean(3, u.channelFlags.isNode1)
+      statement.setBoolean(4, u.channelFlags.isEnabled)
+      statement.setInt(5, u.cltvExpiryDelta.toInt)
+      statement.setLong(6, u.feeBaseMsat.toLong)
+      statement.setLong(7, u.feeProportionalMillionths)
+      statement.setLong(8, u.htlcMinimumMsat.toLong)
+      statement.setLong(9, u.htlcMaximumMsat.toLong)
+      statement.setLong(10, u.timestamp.toLong)
+      statement.setBytes(11, channelUpdateCodec.encode(u).require.toByteArray)
       statement.executeUpdate()
     }
   }
