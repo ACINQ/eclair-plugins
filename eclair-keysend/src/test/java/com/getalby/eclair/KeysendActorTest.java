557d5b3f166b0812b51d579396d22c139e5eede7
kiwiidb <kwinten.debacker@hotmail.com>
2023-04-17 13:41:00 +0200

    keysend experiments in java

--- eclair-keysend/src/test/java/com/getalby/eclair/KeysendActorTest.java
+++ eclair-keysend/src/test/java/com/getalby/eclair/KeysendActorTest.java
@@ -1,5 +1,5 @@
 package com.getalby.eclair;
-
+import org.junit.jupiter.api.Test;
 
 import akka.actor.ActorRef;
 import akka.http.scaladsl.server.RequestContext;
@@ -17,17 +17,13 @@ import fr.acinq.eclair.Setup;
 import fr.acinq.eclair.api.directives.EclairDirectives;
 import fr.acinq.eclair.payment.relay.Relayer;
 import fr.acinq.eclair.payment.send.PaymentInitiator;
+import fr.acinq.eclair.payment.send.PaymentInitiator.SendSpontaneousPayment;
 import fr.acinq.eclair.router.Graph;
 import fr.acinq.eclair.router.Router;
 import fr.acinq.eclair.router.Graph.HeuristicsConstants;
 import fr.acinq.eclair.router.Graph.WeightRatios;
-import fr.acinq.eclair.wire.protocol.GenericTlv;
 import scala.Function1;
 import scala.Option;
-import scala.collection.JavaConverters;
-import scala.collection.Seq;
-import scala.collection.immutable.Nil;
-import scala.collection.mutable.Buffer;
 import scala.concurrent.Future;
 import scala.util.Left;
 import scala.compat.java8.FutureConverters;
@@ -39,31 +35,10 @@ import static akka.http.javadsl.server.Directives.complete;
 
 import static akka.pattern.Patterns.ask;
 
-import java.util.List;
 
-public class KeysendPlugin implements Plugin, RouteProvider {
-
-    private Kit kit;
-    private ActorRef keySendActor;
-
-    @Override
-    public PluginParams params() {
-        return () -> "KeysendPlugin";
-    }
-
-    @Override
-    public void onSetup(Setup setup) {
-
-    }
-
-    @Override
-    public void onKit(Kit kit) {
-        this.keySendActor = kit.system().actorOf(KeysendActor.props());
-        this.kit = kit;
-    }
-
-    @Override
-    public Function1<RequestContext, Future<RouteResult>> route(EclairDirectives directives) {
+public class KeysendActorTest {
+	@Test
+    public void testCreateKeysendClass() {
         Router.SearchBoundaries boundaries = new Router.SearchBoundaries(new MilliSatoshi(1000L), 5.0, 1, new CltvExpiryDelta(10));
         scala.util.Either<WeightRatios, HeuristicsConstants> heuristics = new Left<>(new Graph.WeightRatios(1.0, 0.0, 0.0, 0.0, new Relayer.RelayFees(new MilliSatoshi(100), 100)));
         Router.MultiPartParams mpp = new Router.MultiPartParams(new MilliSatoshi(1000L), 2);
@@ -71,6 +46,16 @@ public class KeysendPlugin implements Plugin, RouteProvider {
                 boundaries,
                 heuristics,
                 mpp, "", false);
+        //PaymentInitiator.SendSpontaneousPayment ssp = new PaymentInitiator.SendSpontaneousPayment(
+        //        new MilliSatoshi(690000L),
+        //        new PublicKey(PublicKey.fromHex("037e9d070515c8ba32e6a32fa698e568fc4944a1bb67ae2048947511267202509b")),
+        //        ByteVector32.One(),
+        //        2,
+        //        Option.apply(""),
+        //        params,
+        //        null,
+        //        false
+        //);
         PaymentInitiator.SendSpontaneousPayment ssp = new PaymentInitiator.SendSpontaneousPayment(
             new MilliSatoshi(1000),
             new Crypto.PublicKey(PublicKey.fromHex("031b84c5567b126440995d3ed5aaba0565d71e1834604819ff9c17f5e9d5dd078f")),
@@ -80,15 +65,6 @@ public class KeysendPlugin implements Plugin, RouteProvider {
             params,
             null,
             false);
-        System.out.println(ssp);
-        //final var fut = ask(kit.paymentInitiator(), ssp, 1000L);
-        //final var s = FutureConverters.toJava(fut);
-
-        return path("keysend", () ->
-                complete("dummy")
-        ).asScala();
-    }
-    public Seq<GenericTlv> convertListToSeq(List<GenericTlv> inputList) {
-        return JavaConverters.asScalaIteratorConverter(inputList.iterator()).asScala().toSeq();
+            System.out.println(ssp);
     }
 }
